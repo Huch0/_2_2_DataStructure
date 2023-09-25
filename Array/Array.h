5da8bd65a0db190d303472bf9705686f233011e9
Huch0 <heochiyeong@gmail.com>
2023-09-25 16:16:23 +0900

    Add new Array methods 1. Constructor with an initializer list 2. Capacity and getCapacity() 3. isEmpty(), isFull() 4. resize() : for dynamic array resizing 5. add() : insert an element at a specific index 6. remove() : remove an element at a specific index 7. sort() : Insertion sort in ascending order 8. out_of_range exception handling in set(), get(), remove()

--- Array/Array.h
+++ Array/Array.h
@@ -3,17 +3,35 @@
 #ifndef ARRAY_H
 #define ARRAY_H
 
+#include <initializer_list>
+
 template <typename T>
 class Array {
 private:
     T* array_;       // Pointer to the dynamic array
     int size_;       // Size of the array
+    int capacity_;   // Capacity of the array
 
 public:
     // Constructor to create an array of a given size
-    explicit Array(int size) {
-        size_ = size;
+    explicit Array() {
+        size_ = 0;
+        capacity_ = 10;
         array_ = new T[size_];  // Allocate memory for the array
+    }
+
+    // Constructor to create an array from an initializer list
+    Array(std::initializer_list<T> elements) {
+        size_ = elements.size();
+        capacity_ = size_;
+        array_ = new T[capacity_];
+
+        // Copy elements from the initializer list to the array
+        int i = 0;
+        for (const T& element : elements) {
+            array_[i] = element;
+            i++;
+        }
     }
 
     // Destructor to release the memory when the object is destroyed
@@ -26,23 +44,125 @@ public:
         return size_;
     }
 
+    // Get the capacity of the array
+    int getCapacity() {
+        return capacity_;
+    }
+
     // Set the value of an element at a specific index
-    void set(int index, const T& element) {
+    void set(const int index, const T& element) {
+        if (index < 0 || index >= size_) {
+            throw std::out_of_range("Index out of bounds in Array::set()");
+        }
+
         array_[index] = element;
     }
 
     // Get the value of an element at a specific index
-    T get(int index) {
+    T get(const int index) {
+        if (index < 0 || index >= size_) {
+            throw std::out_of_range("Index out of bounds in Array::get()");
+        }
+
         return array_[index];
     }
 
     // Display the elements of the array
     void display() {
+        std::cout << "size / capacity : " << size_ << " / " << capacity_ << std::endl;
         std::cout << "[" << array_[0];
         for (int i = 1; i < size_; i++) {
             std::cout << ", " << array_[i];
         }
         std::cout << "]" << std::endl;
+    }
+
+    // Additional methods ...
+
+    // Check is array empty
+    bool isEmpty() {
+        return !size_;
+    }
+
+    // Check is array full
+    bool isFull() {
+        return size_ == capacity_;
+    }
+
+    // Resize the capacity of array
+    void resize() {
+        capacity_ *= 2; // Double the capacity
+        T* newArray = new T[capacity_];
+
+        // Copy elements of the old array to new one
+        for (int i = 0; i < size_; i++) {
+            newArray[i] = array_[i];
+        }
+
+        // Deallocate memory used by the old array
+        delete[] array_;
+
+        // Update the array pointer
+        array_ = newArray;
+    }
+
+    // Insert an element at a specific index
+    void add(const int index, const T &element) {
+        if (isFull())
+            // if the array is full, resize it.
+            resize();
+
+        // Shift elements to right to make space for the new element
+        for (int i = size_; i > index; i--) {
+            array_[i] = array_[i - 1];
+        }
+
+        // Insert the new element at a specific index
+        array_[index] = element;
+
+        // Update the size
+        size_ += 1;
+    }
+
+    // Remove an element at a specific index
+    T remove(const int index) {
+        if (isEmpty()) {
+            throw std::underflow_error("Array is empty in Array::remove()");
+        }
+
+        if (index < 0 || index >= size_)
+            throw std::out_of_range("Index out of bounds in Array::remove()");
+
+        // Store the element to be removed
+        T removed = array_[index];
+
+        // Shift elements to left to remove space for removing element
+        for (int i = index; i < size_ - 1; i++) {
+            array_[i] = array_[i + 1];
+        }
+
+        // Update the size of the array
+        size_ -= 1;
+
+        return removed;
+    }
+
+    // Sort the array in ascending order with Insertion Sort
+    void sort() {
+        for (int i = 1; i < size_; i++) {
+            T current = array_[i];
+            int j = i - 1;
+
+            // Move elements to the right greater than the current element
+            while (j >= 0 && current < array_[j]) {
+                array_[j + 1] = array_[j];
+                j--;
+            }
+
+            // Insert the current element in its correct position
+            array_[j + 1] = current;
+        }
+
     }
 };
 
--- Array/testArray.cpp
+++ Array/testArray.cpp
@@ -1,20 +1,51 @@
 #include <iostream>
 #include "Array.h"
 
-using namespace std;
-
 int main() {
-    Array<int> myArray = Array<int>(10);
+    // Create an instance of the Array class
+    Array<int> myArray1;
 
-    // Set values in the array
-    for (int i = 0; i < myArray.getSize(); ++i) {
-        myArray.set(i, i * 10);
+    // Test isEmpty() method
+    std::cout << "Is array empty? " << (myArray1.isEmpty() ? "Yes" : "No") << std::endl;
+
+    // Test getSize() method
+    std::cout << "Initial size: " << myArray1.getSize() << std::endl;
+
+    // Test getCapacity() method
+    std::cout << "Initial capacity: " << myArray1.getCapacity() << std::endl;
+
+    // Test add() method
+    for (int i = 0; i < 15; i++) {
+        myArray1.add(i, i * 10);
     }
 
-    myArray.display();
+    // Test isFull() method
+    std::cout << "Is array full? " << (myArray1.isFull() ? "Yes" : "No") << std::endl;
 
-    int value = myArray.get(2);
-    cout << "Element at index 2: " << value << endl;
+    // Test resize() method
+    std::cout << "New capacity after resizing: " << myArray1.getCapacity() << std::endl;
 
-    return 1;
+    // Test set() and get() methods
+    myArray1.set(3, 100); // Set element at index 3
+    std::cout << "Element at index 3: " << myArray1.get(3) << std::endl;
+
+    // Test display() method
+    myArray1.display();
+
+    // Test remove() method
+    std::cout << "Removed element at index 3: " << myArray1.remove(3) << std::endl;
+    std::cout << "Array after removing" << std::endl;
+    myArray1.display();
+
+    // Test sort() method
+    Array<int> myArray2 = {3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5};
+
+    std::cout << "Before sorting" << std::endl;
+    myArray2.display();
+
+    myArray2.sort();
+    std::cout << "After sorting" << std::endl;
+    myArray2.display();
+
+    return 0;
 }
